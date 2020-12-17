c07d3d06e397178f1b2d99ccc3f5d766c7000d00
r09722021 <75692281+r09722021@users.noreply.github.com>
2020-12-17 20:43:34 +0800

    動畫

--- background.py
+++ background.py
@@ -70,12 +70,12 @@ class Block(cocos.sprite.Sprite):
         self.image_anchor = 0 , 0
         x, y = position
         if x == 0:
-            self.scale_x = 4.5
+            self.scale_x = 8
             self.scale_y = 1
         else:
             self.scale_x = 0.5 + random.random()*1.5
-            self.scale_y = min(max(y-50+random.random()*100,50),300)/100
-            self.position = x + 50+ random.random()*100,0
+            self.scale_y = min(max(y-50+random.random()*100,100),300)/100
+            self.position = x + 200+ random.random()*100,0
 
 class Game_BG(cocos.layer.Layer):
     def __init__(self):
